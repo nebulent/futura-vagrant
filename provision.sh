c97daa79898e8ce9fb4eeefb506989e63a97c425
Roman Roibu <roman.roibu@gmail.com>
2013-07-17 14:43:19 +0300

    Add admin auth panel database config

--- provision.sh
+++ provision.sh
@@ -12,10 +12,15 @@ export DEBIAN_FRONTEND=noninteractive
 echo "Installing the software..."
 apt-get install -y build-essential git curl libxslt-dev libxml2-dev libcurl3-dev mysql-client libmysqlclient-dev nodejs mysql-server xvfb firefox
 
-echo "Setting up the database from futura_dev.sql seed..."
+echo "Setting up the backend database from futura_dev.sql seed..."
 echo "create database futura_dev; create database futura_test;" | mysql -uroot
 mysql -uroot futura_dev < /vagrant/futura_dev.sql
 mysql -uroot futura_test < /vagrant/futura_dev.sql
+
+echo "Setting up the auth server database from futura_auth_dev.sql seed..."
+echo "create database futura_auth_dev; create database futura_auth_test;" | mysql -uroot
+mysql -uroot futura_auth_dev < /vagrant/futura_auth_dev.sql
+mysql -uroot futura_auth_test < /vagrant/futura_auth_dev.sql
 
 echo "Running init.sh script..."
 su vagrant -c "/bin/bash /vagrant/init.sh"
